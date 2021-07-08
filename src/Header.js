66644861c073c662877af22ea1ab5f5f73ba7c27
Fredrik <fredrikf2000@gmail.com>
2021-07-08 19:04:54 +0200

    finished header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,43 @@
-import './Header.css'
+import "./Header.css";
+import { IconButton, Avatar } from "@material-ui/core";
+import {
+  Apps,
+  ArrowDropDown,
+  Notifications,
+  Search,
+  Menu,
+} from "@material-ui/icons";
 
 function Header() {
-    return (
-        <div>
-            <h2>This is a header</h2>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <Menu />
+        </IconButton>
+        <img
+          src="https://storage.tweak.dk/computer/S%C3%A5dan%20filtreres%20e-mails%20i%20Gmail/gmail%20filtre%20guide.jpg"
+          alt="Gmail"
+        />
+      </div>
+      <div className="header__middle">
+        <Search />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDown className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <Apps />
+        </IconButton>
+        <IconButton>
+          <Notifications />
+        </IconButton>
+        <IconButton>
+          <Avatar />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
